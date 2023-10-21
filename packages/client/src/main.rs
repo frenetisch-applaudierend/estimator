d473db9f1b3d64295a872807db07a30c02dcbcab
Markus Gasser <markus.gasser@mailbox.org>
2023-10-22 01:47:28 +0200

    [WIP] Start with client side authentication

--- packages/client/src/main.rs
+++ packages/client/src/main.rs
@@ -1,11 +1,35 @@
-use ui::ClientApp;
+use std::rc::Rc;
+
+use ui::{
+    auth::{AuthenticationHandler, User},
+    util::ServiceProp,
+    ClientApp, ClientAppProps,
+};
 
 fn main() {
-    let renderer = yew::Renderer::<ClientApp>::new();
+    wasm_logger::init(wasm_logger::Config::default());
+
+    let auth_handler = ClientAuthenticationHandler;
+    let props = ClientAppProps {
+        auth_handler: ServiceProp(Rc::new(auth_handler)),
+    };
+    let renderer = yew::Renderer::<ClientApp>::with_props(props);
 
     if cfg!(profile = "release") {
         renderer.hydrate();
     } else {
         renderer.render();
+    }
+}
+
+struct ClientAuthenticationHandler;
+
+impl AuthenticationHandler for ClientAuthenticationHandler {
+    fn get_user(&self) -> Option<User> {
+        todo!()
+    }
+
+    fn set_user(&self, user: Option<User>) {
+        todo!()
     }
 }
