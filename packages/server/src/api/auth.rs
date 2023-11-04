63145cf912b329b8c65b6bf87d2486d15b8c305b
Markus Gasser <markus.gasser@mailbox.org>
2023-11-04 15:48:28 +0100

    [wip] auth

--- packages/server/src/api/auth.rs
+++ packages/server/src/api/auth.rs
@@ -1,11 +1,46 @@
-use poem_openapi::{payload::PlainText, OpenApi};
+use std::collections::BTreeMap;
 
-pub struct Api;
+use hmac::Hmac;
+use jwt::SignWithKey;
+use poem::web::cookie::{Cookie, CookieJar, SameSite};
+use poem_openapi::{payload::PlainText, OpenApi};
+use sha2::Sha256;
+
+pub struct Api {
+    pub jwt_key: Hmac<Sha256>,
+}
 
 #[OpenApi]
 impl Api {
+    /// Authenticate a user
     #[oai(path = "/auth/login", method = "post")]
-    async fn login(&self) -> PlainText<&'static str> {
-        PlainText("auth/login")
+    async fn login(&self, cookies: &CookieJar) -> PlainText<String> {
+        let mut claims = BTreeMap::<&'static str, String>::new();
+        claims.insert("sub", "markus.gasser@mailbox.org".to_string());
+
+        let token = match claims.sign_with_key(&self.jwt_key) {
+            Ok(token) => token,
+            Err(err) => {
+                tracing::error!("Could not sign JWT: {err}");
+                return PlainText("Error".to_string());
+            }
+        };
+
+        let sig_separator = token.rfind('.').expect("Token should have a signature");
+        let claims_part = &token[0..sig_separator];
+        let sig_part = &token[sig_separator + 1..];
+
+        let mut claims_cookie = Cookie::new_with_str("estimator_auth.claims", claims_part);
+        claims_cookie.set_http_only(false);
+        claims_cookie.set_same_site(SameSite::Strict);
+
+        let mut sig_cookie = Cookie::new_with_str("estimator_auth.sig", sig_part);
+        sig_cookie.set_http_only(true);
+        sig_cookie.set_same_site(SameSite::Strict);
+
+        cookies.add(claims_cookie);
+        cookies.add(sig_cookie);
+
+        PlainText(token)
     }
 }
--- packages/server/src/api/mod.rs
+++ packages/server/src/api/mod.rs
@@ -1,3 +1,4 @@
+use hmac::Mac;
 use poem::{Endpoint, Route};
 use poem_openapi::{OpenApi, OpenApiService};
 
@@ -5,8 +6,10 @@ mod auth;
 
 pub fn routes(uri: &str) -> impl Endpoint {
     let uri = uri.replace("0.0.0.0", "localhost");
-    let service =
-        OpenApiService::new(auth::Api, "Estimator API", "1.0").server(format!("http://{uri}/api"));
+    let jwt_key = hmac::Hmac::<sha2::Sha256>::new_from_slice(b"Some-Secret")
+        .expect("Could not create test secret");
+    let service = OpenApiService::new(auth::Api { jwt_key }, "Estimator API", "1.0")
+        .server(format!("http://{uri}/api"));
     let ui = service.swagger_ui();
 
     Route::new().nest("/", service).nest("/swagger", ui)
